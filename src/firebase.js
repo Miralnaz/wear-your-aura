0aa297ec5230f2a276d4b6afb2a6aa66a256b6f5
MIRAL NAZ <miralnaz672@gmail.com>
2026-04-17 13:52:40 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Ye data aapko Firebase Console se milega
+// Aapka personal config jo screenshot mein hai
 const firebaseConfig = {
-  apiKey: "AIzaSy...", 
-  authDomain: "wear-your-aura.firebaseapp.com",
-  projectId: "wear-your-aura",
-  storageBucket: "wear-your-aura.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:12345:web:abc"
+  apiKey: "AIzaSyAHTELZ7LOCbn99HLYj6I7FGtySiBQzEcE",
+  authDomain: "hn-scents.firebaseapp.com",
+  projectId: "hn-scents",
+  storageBucket: "hn-scents.firebasestorage.app",
+  messagingSenderId: "1003859021492",
+  appId: "1:1003859021492:web:b8d07a6be37f791d3f51e1",
+  measurementId: "G-5N12ZNEYEF"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Database (Firestore) ko export karein taake AdminDashboard use kar sakay
 export const db = getFirestore(app);
